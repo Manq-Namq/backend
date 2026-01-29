5421cff69c2d91dbfc434eeeaf8392f41fa346cf
luciana <belenorosco50@gmail.com>
2026-01-29 17:37:10 -0300

    temporal

--- Usuarios/users.js
+++ Usuarios/users.js
@@ -54,11 +54,19 @@ router.put('/:id', middleware, function(req, res, next) {
     return res.status(403).json({ error: "No tienes permiso para actualizar este perfil" });
   }
   
+
+  const { nombre, apellido, email, telefono, direccion, id_rol } = req.body;
+
+  const sql = "UPDATE usuarios SET nombre = ?, apellido = ?, email = ?, telefono = ?, direccion = ?, id_rol = ? WHERE id_usuario = ?";
+
+  db.query(sql, [nombre, apellido, email, telefono, direccion, id_rol, id])
+
   const { nombre, apellido, email, telefono, direccion } = req.body;
   
   const sql = "UPDATE usuarios SET nombre = ?, apellido = ?, email = ?, telefono = ?, direccion = ? WHERE id_usuario = ?";
   
   db.query(sql, [nombre, apellido, email, telefono, direccion, id])
+
     .then(() => {
       res.json({ mensaje: "Usuario actualizado correctamente" });
     })
